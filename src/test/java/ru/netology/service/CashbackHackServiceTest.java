b6b2a4bb7e68d4b2ecd4b04cfadc40b292adb86e
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-03-16 21:06:15 +0300

    added branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package ru.netology.service;
+
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void shouldReturn1000IfAmountIs2000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(2000);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn999IfAmountIs1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+
+    }
+
+}
