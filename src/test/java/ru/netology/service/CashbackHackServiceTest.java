5b3e680509208f0a8ede39a1bed3ab0fa11873fa
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-03-17 22:30:45 +0300

    added branch junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,21 +1,13 @@
 package ru.netology.service;
 
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class CashbackHackServiceTest {
 
-    @org.junit.Test
-    public void shouldReturn1000IfAmountIs2000() {
-        CashbackHackService service = new CashbackHackService();
-
-        int actual = service.remain(2000);
-        int expected = 1000;
-
-        assertEquals(expected, actual);
-
-    }
 
     @org.junit.Test
     public void shouldReturn0IfAmountIs1000() {
@@ -41,6 +33,40 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void shouldReturn0IfAmountIs1000_API() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturn999IfAmountIs1001_API() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturn1IfAmountIs999_API() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(999);
